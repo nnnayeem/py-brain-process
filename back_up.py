7393f92c93cd9ae457ac389d1d66b6e0438bbd8c
nnnayeem <33355656+nnnayeem@users.noreply.github.com>
2022-05-14 00:40:20 +0600

    live data send to transmitter done

--- back_up.py
+++ back_up.py
@@ -20,21 +20,24 @@ class Main:
 
         with serial.Serial('COM4', 115200, timeout=1) as ser:    
             print("Found serial port com1")
-            i = 0
+            attempt = 0
             while 1:
-                if ser.in_waiting > 0:
-                    data = ser.read(5)
-                    data = data.decode('utf-8')
-                    try:
-                        val = int(data)
-                    except ValueError:
-                        val = 0
+                if attempt == 5: break
+                fileIndex = input("Enter data save index (1-5) To initiate compair mode press c:")
+                filename = "data.{findex}.txt".format(findex = fileIndex)
 
-                    scp.set(val)
-                    i = i + 1
-                    print(i)
-                    if i == 200: break
-                time.sleep(0.01)
+                i = 0
+                while 1:
+                    if ser.in_waiting > 0:
+                        data = ser.read(5)
+                        data = data.decode('utf-8')
+                        with open(filename, 'a') as out:
+                            out.write(data)
+                        i = i + 1
+                        print(i)
+                        if i == 200: break
+                    time.sleep(0.01)
+                attempt = attempt + 1
 
 
 if __name__ == "__main__":
